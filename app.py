8a49c2ce2d444690b94c530c69f492a9dd16af58
Aleksander Petrovskii <petrovskjy.aleksander@gmail.com>
2023-09-04 20:57:24 +0300

    добавили выпадающее меню для выбора сервера

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from resowners.database_helper import get_total_records, get_records
+from resowners.database_helper import get_total_records, get_records, get_servers
 
 app = Flask(__name__)
 
@@ -7,14 +7,17 @@ LIMIT = 30  # количество записей на одной страниц
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    page = request.args.get('page', 1, type=int)
-    offset = (page - 1) * LIMIT
+
+    page = request.args.get('page', 1, type=int) # к-во страниц
+    offset = (page - 1) * LIMIT # смещение
 
     total = get_total_records()
     data = get_records(LIMIT, offset)
 
     pages = range(1, total // LIMIT + 2)
-    return render_template('home.html', data=data, pages=pages, current_page=page)
+    servers = get_servers()
+    return render_template('home.html', data=data, pages=pages, current_page=page, servers=servers)
+
 
 
 if __name__ == '__main__':
--- database_helper.py
+++ database_helper.py
@@ -12,15 +12,21 @@ DATABASE_CONFIG = {
 
 def get_connection():
     return psycopg2.connect(**DATABASE_CONFIG)
-
+# получим датафрейм по всем записям
 def get_total_records():
     with get_connection() as conn:
         with conn.cursor() as cursor:
             cursor.execute("SELECT COUNT(*) FROM vw_tmp_import")
             return cursor.fetchone()[0]
-
+# получим конкретную порцию данных со смещением для вывода в пагинатор
 def get_records(limit, offset):
     with get_connection() as conn:
         with conn.cursor() as cursor:
             cursor.execute(f"SELECT * FROM vw_tmp_import LIMIT {limit} OFFSET {offset}")
             return cursor.fetchall()
+# получим список серверов для выпадающего меню
+def get_servers():
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT server_name FROM servers")
+            return [row[0] for row in cursor.fetchall()]
