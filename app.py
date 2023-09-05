0b9032da86c2915995e6e22dc6a6c7053627f5c6
Aleksander Petrovskii <petrovskjy.aleksander@gmail.com>
2023-09-05 15:32:11 +0300

    добавлен фильтр вывода по владельцам и серверам

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 from flask import Flask, render_template, request
-from resowners.database_helper import get_total_records, get_records, get_servers
+from resowners.database_helper import get_total_records, get_records, get_servers, get_owners
 
 app = Flask(__name__)
 
 LIMIT = 30  # количество записей на одной странице
 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    page = request.args.get('page', 1, type=int)  # получим текущую страницу
+    offset = (page - 1) * LIMIT  # получим смещение
 
-    page = request.args.get('page', 1, type=int) # получим текущую страницу
-    offset = (page - 1) * LIMIT # получим смещение
-    servers = get_servers() # список серверов для выпадающего меню
-    print(servers)
+    servers = get_servers()  # список серверов для выпадающего меню
     selected_server = request.args.get('server')
-    print(selected_server)
-    if selected_server == "ALL": # обработаем п.меню для возврата к выбору всех серверов
+    if selected_server == "ALL":  # обработаем п.меню для возврата к выбору всех серверов
         selected_server = None
-    total = get_total_records(selected_server)
-    if selected_server:  # Если сервер выбран
-        data = get_records(selected_server, LIMIT, offset)
-    else:  # Если сервер не выбран, выводим все данные
-        data = get_records(None, LIMIT, offset)
 
+    owners = get_owners()  # список владельцев для выпадающего меню
+    selected_owner = request.args.get('owner')
+    if selected_owner == "ALL":
+        selected_owner = None
+
+    total = get_total_records(selected_server)
+    data = get_records(selected_server, selected_owner, LIMIT, offset)
     pages = range(1, total // LIMIT + 2)
+
     return render_template('home.html', data=data, pages=pages, current_page=page, servers=servers,
-                           selected_server=selected_server)
+                           selected_server=selected_server, owners=owners, selected_owner=selected_owner)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database_helper.py
+++ database_helper.py
@@ -27,20 +27,32 @@ def get_total_records(server_name=None):
                 )
             return cursor.fetchone()[0]
 
-# получим конкретную порцию данных со смещением для вывода в пагинатор
-def get_records(server_name, limit, offset):
+def get_records(server_name, owner_name, limit, offset):
+    """
+     ф. обрабатывает все возможные комбинации выбора сервера и владельца:
+    1. Ни сервер, ни владелец не выбраны: выводятся все записи.
+    2. Выбран только сервер: выводятся записи только для этого сервера.
+    3. Выбран только владелец: выводятся записи только для этого владельца.
+    4. Выбраны и сервер, и владелец: выводятся записи только для этой комбинации сервера и владельца.
+    """
     with get_connection() as conn:
         with conn.cursor() as cursor:
-            if server_name == "ALL" or not server_name: # обрабоатем вып. меню
-                cursor.execute(
-                    f"SELECT * FROM vw_tmp_import LIMIT {limit} OFFSET {offset}"
-                )
-            else: # в меню выбран конкретный сервер
-                cursor.execute(
-                    f"SELECT * FROM vw_tmp_import WHERE server_name = %s LIMIT {limit} OFFSET {offset}",
-                    (server_name,)
-                )
+            if server_name == "ALL" or not server_name:
+                if owner_name and owner_name != "ALL":
+                    query = f"SELECT * FROM vw_tmp_import WHERE full_name_owner = %s LIMIT {limit} OFFSET {offset}"
+                    cursor.execute(query, (owner_name,))
+                else:
+                    query = f"SELECT * FROM vw_tmp_import LIMIT {limit} OFFSET {offset}"
+                    cursor.execute(query)
+            else:
+                if owner_name and owner_name != "ALL":
+                    query = f"SELECT * FROM vw_tmp_import WHERE server_name = %s AND full_name_owner = %s LIMIT {limit} OFFSET {offset}"
+                    cursor.execute(query, (server_name, owner_name))
+                else:
+                    query = f"SELECT * FROM vw_tmp_import WHERE server_name = %s LIMIT {limit} OFFSET {offset}"
+                    cursor.execute(query, (server_name,))
             return cursor.fetchall()
+
 
 
 # получим список серверов для выпадающего меню
@@ -48,4 +60,10 @@ def get_servers():
     with get_connection() as conn:
         with conn.cursor() as cursor:
             cursor.execute("SELECT DISTINCT server_name FROM vw_tmp_import")
+            return cursor.fetchall()
+# получии список владельцев для выпадающего меню
+def get_owners():
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT DISTINCT full_name_owner FROM vw_tmp_import")
             return cursor.fetchall()
